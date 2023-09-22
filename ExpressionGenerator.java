492e2932e043e2506536205e7c1c59d1727a6457
abdelhamid.mouti <abdelhamid.mouti@epita.fr>
2023-09-22 13:52:49 +0200

    valid expression generator

--- ExpressionGenerator.java
+++ ExpressionGenerator.java
@@ -14,7 +14,7 @@ public class ExpressionGenerator {
 
         String path = "C:/Users/USER/Desktop/JAVA training/fourEqualsTen/";
 
-        FileOutputStream fos = null;
+        FileOutputStream fos;
         try {
             fos = new FileOutputStream(path + "result.txt");
         } catch (FileNotFoundException e) {
@@ -29,14 +29,16 @@ public class ExpressionGenerator {
 
         List<String> numberPermutations = new ArrayList<>();
 
-        numberPermutations.add("12(34)");
         numberPermutations.add("1(234)");
-        numberPermutations.add("1(32)4");
-        numberPermutations.add("(123)4");
 
 
         List<String> operatorPermutations = OperatorPermutations.generateOperatorPermutations();
 
+
+        List<String> res = generateExpressions(operatorPermutations,numberPermutations);
+
+        for (String s: res)
+            System.out.println(s);
 
 
         try {
@@ -48,12 +50,43 @@ public class ExpressionGenerator {
 
     }
 
-    public List<String> generateExpressions(List<String> operatorPermutations, List<String> numberPermutations)
-    {
+    public static List<String> generateExpressions(List<String> operatorPermutations, List<String> numberPermutations) {
         List<String> expressions = new ArrayList<>();
+
+        for (String permutationNum : numberPermutations)
+        {
+            for(String ops : operatorPermutations )
+            {
+                expressions.add(generateExpressionsHelper(ops,permutationNum));
+            }
+        }
+        return expressions;
 
     }
 
 
+    public static String generateExpressionsHelper(String operatorPermutation, String numberPermutations) {
+
+        //1(23)4
+        StringBuilder res = new StringBuilder();
+        StringBuilder ops = new StringBuilder(operatorPermutation);
+        for (int i =0; i<numberPermutations.length();i++)
+        {
+            char c =numberPermutations.charAt(i);
+            // Check conditions for appending the character
+            if ((Character.isDigit(c) && (i != numberPermutations.length() -1 && (Character.isDigit(numberPermutations.charAt(i + 1)) ||
+                    numberPermutations.charAt(i + 1) == '(' ) ) ) || c == ')' && i != numberPermutations.length() -1) {
+
+                res.append(c);
+                res.append(ops.charAt(0));
+                ops.deleteCharAt(0);
+                continue;
+            }
+            res.append(c);
+        }
+
+        return res.toString();
+    }
 }
+
 
